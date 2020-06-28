e1d53ce527bf86e0efbf2de07a57a9ba06d9e00b
Ugur Guney <ugurguney@gmail.com>
2020-06-27 21:33:49 -0400

    Class for cached implicit graph BFS traversel.
    Graph class takes function that defines the implicit relationships of vertices.
    `compute_successors` returns the list of successors of a vertex.
    While traversing, adjacency information is stored in a cache and reused instead of computing successors multiple times.
    
    Output of the example in main2()
    
    ```
    ['1/2 1/4 1/4', '1/4 1/4 1/4 1/4', '1/2 1/8 1/8 1/4', '1/2 1/4 1/8 1/8', '1/2 1/6 1/6 1/6']
    1 ['1/2 1/2']
    1/2 1/2 ['1/4 1/4 1/2', '1/2 1/4 1/4', '1/3 1/3 1/3']
    1/4 1/4 1/2 ['1/8 1/8 1/4 1/2', '1/4 1/8 1/8 1/2', '1/4 1/4 1/4 1/4', '1/6 1/6 1/6 1/2']
    1/2 1/4 1/4 ['1/4 1/4 1/4 1/4', '1/2 1/8 1/8 1/4', '1/2 1/4 1/8 1/8', '1/2 1/6 1/6 1/6']
    1/3 1/3 1/3 ['1/6 1/6 1/3 1/3', '1/3 1/6 1/6 1/3', '1/3 1/3 1/6 1/6', '2/9 2/9 2/9 1/3', '1/3 2/9 2/9 2/9']
    ```

--- cac.py
+++ cac.py
@@ -1,7 +1,21 @@
 from collections import deque
-from typing import Any, Callable, Set, List, Tuple, Hashable, Generic, TypeVar, cast
+from typing import (
+    Any,
+    Callable,
+    cast,
+    Dict,
+    List,
+    Generic,
+    Hashable,
+    Set,
+    Tuple,
+    TypeVar,
+)
 
 from music21.pitch import Pitch
+import networkx as nx
+from networkx import convert
+
 
 class Triplet(object):
     """A container of 3 Pitches.
@@ -48,3 +62,67 @@ def construct_graph(
         steps -= 1
     return vertices, edges
 
+
+TVertex = TypeVar("TVertex", bound=Hashable)
+
+
+class Graph(Generic[TVertex]):
+    def __init__(
+        self,
+        compute_successors: Callable[[TVertex], List[TVertex]],
+        pre_process: Callable[[TVertex], Any] = None,
+        post_process: Callable[[TVertex], Any] = None,
+        process_edge: Callable[[TVertex, TVertex], Any] = None,
+    ):
+        self._compute_successors = compute_successors
+        self.vertices: Set[TVertex] = set()
+        self.pre_process = pre_process
+        self.post_process = post_process
+        self.process_edge = process_edge
+        self.adjacency: Dict[TVertex, List[TVertex]] = {}
+
+    def get_successors(self, u: TVertex) -> List[TVertex]:
+        """Get successors in memoized manner.
+
+        If they are already computed, retrieve them from self.adjacency cache.
+        Otherwise, compute and cache them.
+        """
+        if u not in self.adjacency:
+            self.adjacency[u] = self._compute_successors(u)
+        return self.adjacency[u]
+
+    def traverse(self, start: TVertex, distance: int = 0) -> List[TVertex]:
+        """Get the list of all vertices that are within distance away from start.
+
+        While traversing caches edges in self.adjacency.
+        distance = 0 returns vertex itself, however, outgoing edges are cached.
+        distance = 1 returns vertex itself and its neighbors. Again outgoing
+        edges from neighbors are cached.
+        """
+        traversed = []
+        seen: Set[TVertex] = set([start])
+        processed: Set[TVertex] = set()
+
+        q = deque([start])
+        while q and distance >= 0:
+            for i in range(len(q)):  # trick to keep track of distance
+                u = q.pop()
+                traversed.append(u)
+                if self.pre_process:
+                    self.pre_process(u)
+                successors = self.get_successors(u)
+                processed.add(u)
+                for v in successors:
+                    if self.process_edge and v not in processed:
+                        self.process_edge(u, v)
+                    if v not in seen:
+                        q.appendleft(v)
+                        seen.add(v)
+                if self.post_process:
+                    self.post_process(u)
+            distance -= 1
+        return traversed
+
+    def get_networkx_graph(self) -> nx.DiGraph:
+        """make a nx graph from processed vertices/edges."""
+        return convert.from_dict_of_lists(self.adjacency)
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from collections import deque
-from typing import Callable, Tuple, Set
+from typing import Set
 
 from music21.pitch import Pitch
 from music21.scale import MajorScale
 import networkx as nx
 
+from cac import construct_graph, Triplet
 from discoversounds import (
     query_sound,
     Section,
@@ -16,38 +16,36 @@ from midi import Player
 from visualization import plot_hierarchical_graph
 
 
-all_pitches = MajorScale(tonic="C").getPitches("C1", "C8")
-pitch2index = {p: ix for ix, p in enumerate(all_pitches)}
-C2 = Pitch("C2")
-C3 = Pitch("C3")
-C4 = Pitch("C4")
-C5 = Pitch("C5")
-C6 = Pitch("C6")
+def main1():
+    all_pitches = MajorScale(tonic="C").getPitches("C1", "C8")
+    pitch2index = {p: ix for ix, p in enumerate(all_pitches)}
+    C2 = Pitch("C2")
+    C3 = Pitch("C3")
+    C4 = Pitch("C4")
+    C5 = Pitch("C5")
+    C6 = Pitch("C6")
 
+    def get_next_triplets(triplet: Triplet) -> Set[Triplet]:
+        next_triplets = set()
+        for i, pitch in enumerate(triplet.pitches):
+            # TODO: improve construct_graph to accept negative values here
+            for diff in [1]:
+                # TODO: make this a utility function
+                moved_pitch = all_pitches[pitch2index[pitch] + diff]
+                if moved_pitch in triplet.pitches:
+                    continue
+                next_pitches = tuple(
+                    p if i != j else moved_pitch for j, p in enumerate(triplet.pitches)
+                )
+                if (
+                    not C2 <= next_pitches[0] <= C4
+                    or not C3 <= next_pitches[1] <= C5
+                    or not C4 <= next_pitches[2] <= C6
+                ):
+                    continue
+                next_triplets.add(Triplet(next_pitches))
+        return next_triplets
 
-def get_next_triplets(triplet: Triplet) -> Set[Triplet]:
-    next_triplets = set()
-    for i, pitch in enumerate(triplet.pitches):
-        # TODO: improve construct_graph to accept negative values here
-        for diff in [1]:
-            # TODO: make this a utility function
-            moved_pitch = all_pitches[pitch2index[pitch] + diff]
-            if moved_pitch in triplet.pitches:
-                continue
-            next_pitches = tuple(
-                p if i != j else moved_pitch for j, p in enumerate(triplet.pitches)
-            )
-            if (
-                not C2 <= next_pitches[0] <= C4
-                or not C3 <= next_pitches[1] <= C5
-                or not C4 <= next_pitches[2] <= C6
-            ):
-                continue
-            next_triplets.add(Triplet(next_pitches))
-    return next_triplets
-
-
-def main():
     init_pitches = (Pitch("C2"), Pitch("C3"), Pitch("C4"))
     init_triplet = Triplet(init_pitches)
     _, edges = construct_graph(init_triplet, get_next_triplets, 48)
@@ -106,5 +104,67 @@ def main():
     p.play()
 
 
+def main2():
+    from fractions import Fraction
+    from typing import List, Tuple
+
+    from cac import Graph
+
+    Pattern = Tuple[Fraction, ...]
+    # class Pattern(Tuple[int, ...]):
+    #     def __str__(self):
+    #         return " ".join([str(x) for x in self])
+
+    def split_notes(pat: Pattern) -> List[Pattern]:
+        successors = []
+        # rule 1: single item can be replace with two items with 1/2 length
+        for i in range(len(pat)):
+            one = pat[i : i + 1]
+            if True:
+                double = (one[0] / 2,) * 2
+                # succ = Pattern(pat[:i] + double + pat[i + 1 :])
+                succ = pat[:i] + double + pat[i + 1 :]
+                successors.append(succ)
+        for i in range(len(pat) - 1):
+            two = pat[i : i + 2]
+            if two[0] == two[1]:
+                triplet = (two[0] / 3 * 2,) * 3
+                succ = pat[:i] + triplet + pat[i + 2 :]
+                successors.append(succ)
+        for i in range(len(pat) - 3):
+            four = pat[i : i + 4]
+            if four[0] == four[1] == four[2] == four[3]:
+                quintlet = (four[0] / 5 * 4,) * 5
+                succ = pat[:i] + quintlet + pat[i + 4 :]
+                successors.append(succ)
+        return successors
+
+    # traverse one step from selected child at a time
+    g1 = Graph[Pattern](split_notes)
+    g1.traverse(start=(Fraction(1, 1),), distance=1)
+    g1.traverse(start=(Fraction(1, 2), Fraction(1, 2)), distance=1)
+    results = g1.traverse(
+        start=(Fraction(1, 2), Fraction(1, 4), Fraction(1, 4)), distance=1
+    )
+
+    # traverse all children in given distance
+    g2 = Graph[Pattern](split_notes)
+    g2.traverse(start=(Fraction(1, 1),), distance=2)
+
+    def f2s(frs):
+        return " ".join([str(fr) for fr in frs])
+
+    print([f2s(u) for u in results])
+
+    for u, vs in g2.adjacency.items():
+        print(
+            f2s(u), [f2s(v) for v in vs],
+        )
+
+    # G = g.get_networkx_graph()
+    # print([f2s(nd) for nd in G.nodes], [(f2s(u), f2s(v)) for u, v in G.edges])
+    # print(list(G.adjacency()))
+
+
 if __name__ == "__main__":
-    main()
+    main2()
